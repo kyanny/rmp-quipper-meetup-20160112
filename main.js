8fe6760c581b6cde5e106cf37242d3b6171e6ce2
Kensuke Nagae <kyanny@gmail.com>
2016-01-12 00:31:02 +0900

    Timer

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 jQuery(function(){
+  var max = 5 * 60;
+  var $stopwatch = $('button.btn-default');
+  $stopwatch.text('05:00');
+  var duration = 0;
+  setInterval(function() {
+    duration++;
+    var min = Math.floor((max - duration) / 60);
+    var sec = Math.round((max - duration) % 60);
+    if (min === 0 && sec === 0) {
+      location.href = 'http://www.recruit-mp.co.jp/career_engineer/';
+    }
+    if (min < 10) {
+      min = '0' + min;
+    }
+    if (sec < 10) {
+      sec = '0' + sec;
+    }
+    $stopwatch.text(min + ':' + sec);
+  }, 1000);
+
   var len = $('.roulette img').length;
   var stopped = [];
   function getStopImageNumber() {
